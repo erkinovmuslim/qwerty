c0db40352c404051d6510b7996a691d31956ec76
erkinovmuslim <113027072+erkinovmuslim@users.noreply.github.com>
2022-09-07 16:47:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$("#scale-0").click(function() {
+  $("#scale-0").addClass('button-clicked');
+});
+$("#scale-1").click(function() {
+  $("#scale-1").addClass('button-clicked');
+});
+$("#scale-2").click(function() {
+  $("#scale-2").addClass('button-clicked');
+});
+$("#scale-3").click(function() {
+  $("#scale-3").addClass('button-clicked');
+});
+
